7f057d2a6a89d9c1cae523a20b46d08314c199d4
ARJYoung <adamy044@gmail.com>
2024-12-03 21:45:25 +0000

    commit changes 3

--- src/components/Product3/Product3.js
+++ src/components/Product3/Product3.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
 function Product3() {
-  return <div></div>;
+  return <div>    <h1>Tombstone Soda</h1>
+  <div class="row">
+    <div class="col-md text">
+<p>To error is human, to forgive is divine
+Well I'm not forgivin' and the error ain't mine
+They ain't quite human
+At least they're not to my eyes
+One sip of this and I'll give 'em the shock of their lives
+Yea, we got the guns and we got the bombs
+In deadshot heaven we pop, skulls for fun
+Zero in baby, zero on that spot
+The hot spot baby, give 'em, all you got
+So quit, complainin'
+About your bad aimin
+Just try, try again for me
+With the headshot power of  daiquiri! </p>
+    </div>
+    <div class="col-md">
+    <img src={tombstoneLogo} className='image'/>
+    </div>
+    <div class="col-md text">
+      
+<p>A st
+  </p>
+    </div>
+    </div>
+
+  <br/>
+</div>;
 }
 
 export default Product3;
